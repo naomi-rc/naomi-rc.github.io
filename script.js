df95dc3bbd0f641a31b790f37a5c98bbb3c505cf
naomi-rc <naomi.cat182128@gmail.com>
2019-04-06 20:38:22 -0400

    completed modal and added spaceinvaders project

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-var headings = ["Twenty-One", "Hangman", "Test"];
-var images = ["images/twenty-one.png", "images/hangman.png"];
-var dates = ["January 2019", "January 2018"];
-var contents = ["A web-based version of the card game, Twenty One. Play against three computer-players and see if you can win.",
+var headings = ["Space Invaders", "Hibisca", "Twenty-One", "Hangman"];
+var images = ["images/space_invaders.png", "images/hibisca_poster.jpg", "images/twenty-one.png", "images/hangman.png"];
+var dates = ["November 2018", "January - May 2017","January 2019", "January 2018"];
+var contents = ["A remake of the popular Space Invaders game from the 1970s",
+				"Take care of plants using Android sensors, notifications, and news and music APIs.",
+				"A web-based version of the card game, Twenty One. Play against three computer-players and see if you can win.",
 				"A web-based version of Hangman. Sound effects and music play in the background.",
 				"https://naomi-rc.github.io/hangman/"
 				];
-var links = ["https://naomi-rc.github.io/twenty-one/", "https://naomi-rc.github.io/hangman/", ""];
+var links = ["", "","https://naomi-rc.github.io/twenty-one/", "https://naomi-rc.github.io/hangman/", ""];
+var srcs = ["images/space_invaders1.png", "images/hibisca_poster_original.jpg", "", ""];
 
 
 /**
@@ -22,7 +25,7 @@ const init = function init(){
 			link = elementHelper("a", {"href":links[index], "target":"blank", "class":"btn btn-primary"}, "View "+headings[index]);
 		}
 		else{
-			link = elementHelper("a", {"src":"images/hangman.png", "class":"btn btn-primary open-gallery"}, "View "+headings[index]);
+			link = elementHelper("span", {"src":srcs[index], "href":"", "class":"btn btn-primary open-gallery"}, "View "+headings[index]);
 		}
 
 		var cardBody = elementHelper("div", {"class":"card-body"}, "", [title, date, content, link]);
